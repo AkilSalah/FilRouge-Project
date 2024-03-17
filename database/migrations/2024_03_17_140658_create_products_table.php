dd5103ccefe809a58795500187390bfc73d63db1
root <root@LAPTOP-EQH4NAMI>
2024-03-17 15:29:55 +0100

    create migrations

---
+++ database/migrations/2024_03_17_140658_create_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('productName');
+            $table->string('productImage');
+            $table->string('productDescription');
+            $table->integer('productPrice');
+            $table->foreignId('id_Category')->constrained('categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
