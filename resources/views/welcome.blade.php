c0fe024b04b2920571ec0521f23bf4adb91e5f45
root <root@LAPTOP-EQH4NAMI>
2024-04-26 21:16:08 +0200

    search

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -486,8 +486,8 @@
                             <a href="/Client/ProductDetails/${product.id}">
                                 <div class="bg-gray-100 rounded-2xl p-6 hover:-translate-y-2 transition-all relative">
                                     <div class="bg-gray-100 w-10 h-10 flex items-center justify-center rounded-full cursor-pointer absolute top-4 right-4">
-                                        <svg xmlns="http://www.w3.org/2000/svg" width="18px" class="fill-${heartColor}-400 inline-block" viewBox="0 0 64 64">
-                                            <path d="M45.5 4A18.53 18.53 0 0 0 32 9.86 18.5 18.5 0 0 0 0 22.5C0 40.92 29.71 59 31 59.71a2 2 0 0 0 2.06 0C34.29 59 64 40.92 64 22.5A18.52 18.52 0 0 0 45.5 4ZM32 55.64C26.83 52.34 4 36.92 4 22.5a14.5 14.5 0 0 1 26.36-8.33 2 2 0 0 0 3.27 0A14.5 14.5 0 0 1 60 22.5c0 14.41-22.83 29.83-28 33.14Z" data-original="#000000"></path>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="25px" fill="${heartColor}" viewBox="0 0 512 512">
+                                                <path d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/>
                                         </svg>
                                     </div>
                                     <div class="w-11/12 h-[220px] overflow-hidden mx-auto aspect-w-16 aspect-h-8 md:mb-2 mb-4">
